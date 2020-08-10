16f453b7c6d487c5ee90b9c4990b68c4f11d7abd
Stefan Geyer <stefangeyer@outlook.com>
2020-08-10 03:14:54 +0200

    Adds in cylon blueprint and first bit of gesture handling

---
+++ index.js
@@ -0,0 +1,115 @@
+// To run:
+// $ node index.js [IP] [username]
+
+"use strict";
+
+const Cylon = require("cylon");
+
+// Initialize the robot
+// Cylon.robot({
+//     connections: {
+//         leapmotion: { adaptor: "leapmotion" },
+//         hue: { adaptor: 'hue', host: process.argv[2], username: process.argv[3] }
+//     },
+
+//     devices: {
+//         leapmotion: { driver: "leapmotion" },
+//         bulb1: { driver: 'hue-light', lightId: 1 },
+//         bulb2: { driver: 'hue-light', lightId: 2 },
+//         bulb3: { driver: 'hue-light', lightId: 3 }
+//     },
+
+//     work: (my) => {
+//         my.leapmotion.on("gesture", (gesture, frame) => {
+//             if (gesture.type == "swipe") {
+//                 // console.log("swipe gesture");
+//                 // Classify swipe as either horizontal or vertical
+//                 const horizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+//                 let swipeDirection = "";
+//                 //Classify as right-left or up-down
+//                 if (horizontal) {
+//                     if (gesture.direction[0] > 0) {
+//                         swipeDirection = "right";
+//                     } else {
+//                         swipeDirection = "left";
+//                     }
+//                 } else { //vertical
+//                     if (gesture.direction[1] > 0) {
+//                         swipeDirection = "up";
+//                     } else {
+//                         swipeDirection = "down";
+//                         my.bulb1.brightness(0);
+//                     }
+//                 }
+//                 console.log(swipeDirection);
+//             }
+
+//             // console.log(gesture.toString());
+//         })
+//     }
+// }).start();
+
+Cylon.robot({
+    name: "leapbot",
+
+    connections: {
+        leapmotion: { adaptor: "leapmotion" }
+    },
+
+    devices: {
+        leapmotion: { driver: "leapmotion" }
+    },
+
+    work: (my) => {
+        my.leapmotion.on("gesture", (gesture, frame) => {
+            if (gesture.type == "swipe") {
+                // console.log("swipe gesture");
+                // Classify swipe as either horizontal or vertical
+                const horizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+                let swipeDirection = "";
+                //Classify as right-left or up-down
+                if (horizontal) {
+                    if (gesture.direction[0] > 0) {
+                        swipeDirection = "right";
+                    } else {
+                        swipeDirection = "left";
+                    }
+                } else { //vertical
+                    if (gesture.direction[1] > 0) {
+                        swipeDirection = "up";
+                        // TODO throttle / debounce
+                        Cylon.bulb1.brightness(100);
+                    } else {
+                        swipeDirection = "down";
+                        Cylon.bulb1.brightness(0);
+                    }
+                }
+                console.log(swipeDirection);
+            }
+
+            // console.log(gesture.toString());
+        })
+    }
+}).start();
+
+Cylon.robot({
+
+    name: "huebot",
+
+    connections: {
+        hue: { adaptor: 'hue', host: process.argv[2], username: process.argv[3] }
+    },
+
+    devices: {
+        bulb1: { driver: 'hue-light', lightId: 1 },
+        bulb2: { driver: 'hue-light', lightId: 2 },
+        bulb3: { driver: 'hue-light', lightId: 3 }
+    },
+
+    work: (my) => {
+    }
+}).start();
+
+Cylon.bulb1 = Cylon.MCP.robots.huebot.devices.bulb1;
+Cylon.bulb2 = Cylon.MCP.robots.huebot.devices.bulb2;
+Cylon.bulb3 = Cylon.MCP.robots.huebot.devices.bulb3;
